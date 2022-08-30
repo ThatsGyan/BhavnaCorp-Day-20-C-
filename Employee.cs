f52e75eb9233ef41abef5a1bc069a9740c93f67e
GYANENDRA PAL SINGH <46261508+ThatsGyan@users.noreply.github.com>
2022-08-30 14:43:26 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class Employee
+    {
+        public string name { get; set; }
+        public string department { get; set; }
+        public double salary { get; set; }  
+        public string gender { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.SqlTypes;
+using System.Data.SqlClient;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Employee emp = new Employee();
+
+            Console.WriteLine("Enter the name of Employee");
+            emp.name = Console.ReadLine();
+
+            Console.WriteLine("Enter Department of Employee");
+            emp.department = Console.ReadLine();
+
+            Console.WriteLine("Enter Department of Salary");
+            emp.salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Department of Gender");
+            emp.gender = Console.ReadLine();
+
+            //connection string
+
+            SqlConnection con = new SqlConnection("server=localhost;database = Employee;integrated security=true");
+
+            //Command Creation
+
+            //for string data --> ' ""+emp.name + " '
+            SqlCommand cmd = new SqlCommand("insert into Employee values(' " +emp.name+"','"+emp.department+"',"+emp.salary+",'"+emp.gender+"')",con); // command for inserting values in DB table
+
+            
+            con.Open();
+            cmd.ExecuteNonQuery(); //executr cmd
+            con.Close(); //connection close
+
+            Console.WriteLine("Record Inserted Sucessfully");
+
+
+
+        }
+    }
+}
